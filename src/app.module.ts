965dc6d86b1b66f3d39b41e47693803c125307d6
Saul <saulcalderon1408@gmail.com>
2024-09-14 13:28:17 -0600

    chore: Remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MapperModule } from './mapper/mapper.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MapperModule],
 })
 export class AppModule {}
